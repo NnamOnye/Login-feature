74365a32b2086d3cb995a371955465f6636099bf
M.O <84255559+NnamOnye@users.noreply.github.com>
2025-07-01 11:43:24 -0500

    python ver

--- python ver.py
+++ python ver.py
@@ -1,6 +1,7 @@
 #Python User Login Feature
 
 users = {
+#here instead of hardcoding the password you can ask the user for an input --> getinput() and store it 
   "user1": "password1",
   "user2": "password2",
   "user3": "password3"
@@ -13,16 +14,20 @@ def login():
   if username in users:
     print("Welcome enter your password")
     return True
+   
   else:
     print("INCORRECT")
     return False
+    #add action: getinput()
 
   password = input() #will prompt for password
   if users[username] == password:
     print("Access granted")
+    #add action: unlockdoor()
     return True
   else:
     print("Access Denied Re-enter Info Or Exit System")
+    #add action: getinput()
   return False
   
 if __name__ == "__main__":
@@ -31,3 +36,4 @@ if __name__ == "__main__":
     continue
   
   print("Access granted. You can now proceed with your tasks.")
+  #add action: unlockdoor()
